513722462463deff7581a71ab11da4efbd97a2cc
Taeho <k1mta3h0@gmail.com>
2025-10-25 22:33:28 +0900

    [FEAT] lookahead update

--- ISA-32 assembler/parser.hpp
+++ ISA-32 assembler/parser.hpp
@@ -87,6 +87,7 @@ namespace parser_generator {
 		public:
 			u64 prodId;
 			u64 dotPos;
+			u64 setId;
 		};
 
 		class Rule {
@@ -97,42 +98,6 @@ namespace parser_generator {
 
 		template <typename NonTerminal, typename Terminal>
 		using Grammer = std::unordered_map < NonTerminal, std::vector<Element>> ;
-
-		void getFirstTable(
-			std::unordered_map<Element, std::vector<u64>> &table, 
-			std::unordered_multimap<Element, Element>& revFirstGrammmer, 
-			Element terminalEnd) {
-
-			table.clear();
-			size_t termCount = (terminalEnd >> 6) + 1;
-
-			typedef struct _QData {
-				Element cur;
-				Element prev;
-			} QData;
-
-			std::queue<QData> elementQ;
-
-			for (Element i = 0; i < terminalEnd; i++) {
-				std::vector<u64> tmp(termCount, 0);
-				tmp[i >> 6] |= (u64)1 << (i & 0b111111);
-				table[i] = tmp;
-
-				elementQ.push({ i, i });
-			}
-	
-			while (!elementQ.empty()) {
-				QData cur = elementQ.front(); elementQ.pop();
-
-				std::vector<u64>& curV = table[cur.cur], prevV = table[cur.prev];
-				for (size_t i = 0; i < curV.size(); i++)
-					curV[i] |= prevV[i];
-
-				auto range = revFirstGrammmer.equal_range(cur.cur);
-				for (auto it = range.first; it != range.second; ++it)
-					elementQ.push({ it->second, cur.cur });
-			}
-		}
 
 		void getClosure(
 			std::vector<Item>& closure,
@@ -217,7 +182,7 @@ namespace parser_generator {
 
 		void createC0(
 			std::vector<std::vector<Item>>& closureVec,
-			std::unordered_multimap<ID, ID>& transitionMap,
+			std::unordered_multimap<ID, std::pair<ID, Element>>& transitionMap,
 			std::vector<std::pair<Element, std::vector<Element>>>& grammerVec,
 			std::unordered_multimap<Element, std::pair<std::vector<Element>, u64>>& grammerMap,
 			Item startItem) {
@@ -253,25 +218,121 @@ namespace parser_generator {
 						continue;
 
 					ID id = closureId++;
-					transitionMap.insert({ cur.id, id });
+					transitionMap.insert({ cur.id, { id, it->first} });
 
 					setQ.push({ closure, id });
 				}
 			}
 		}
 
-		void getLookahead(
-			std::vector<Element>& lookahead,
-			std::vector<std::vector<Item>>& closureVec,
-			std::unordered_multimap<ID, ID>& transitionMap,
-			std::vector<std::pair<Element, std::vector<Element>>>& grammerVec,
-			std::unordered_multimap<Element, std::pair<std::vector<Element>, u64>>& grammerMap,
-			ID closureID, u64 itemNum) {
+		void getFirstTable(
+			std::unordered_map<Element, std::vector<u64>>& table,
+			std::unordered_multimap<Element, Element>& revFirstGrammmer,
+			Element terminalEnd) {
 
+			table.clear();
+			size_t termCount = (terminalEnd >> 6) + 1;
 
+			typedef struct _QData {
+				Element cur;
+				Element prev;
+			} QData;
+
+			std::queue<QData> elementQ;
+
+			for (Element i = 0; i < terminalEnd; i++) {
+				std::vector<u64> tmp(termCount, 0);
+				tmp[i >> 6] |= (u64)1 << (i & 0b111111);
+				table[i] = tmp;
+
+				elementQ.push({ i, i });
+			}
+
+			while (!elementQ.empty()) {
+				QData cur = elementQ.front(); elementQ.pop();
+
+				std::vector<u64>& curV = table[cur.cur], prevV = table[cur.prev];
+				for (size_t i = 0; i < curV.size(); i++)
+					curV[i] |= prevV[i];
+
+				auto range = revFirstGrammmer.equal_range(cur.cur);
+				for (auto it = range.first; it != range.second; ++it)
+					elementQ.push({ it->second, cur.cur });
+			}
 		}
 
-		void createTable() {
+		// 
+		//	LA(p, [A->x.y]) {
+		//		set = {}
+		//		
+		//		auto range = revTransitionMap[x].equal_range(p);
+		//		for (auto qIt = range.first; qIt != range.second; ++qIt) {
+		//			q = grammerVec[*qIt];
+		//			for (auto it = q.begin(); it != q.end(); ++it) {
+		//				set.insert(firstTable[it->b]);
+		//				if (firstTable[it->b].epslon)
+		//					set.insert(LA(q, [B->a.Ab]));
+		//			}	
+		//		}
+		//	}
+		//	
+		//	q = PRED(p, x);
+		//	[B->aAb] << q;
+		//	LA(p, [A->x.y]) = first(b) + LA(q, [B->aAb]);
+		// 
+
+		void getLookahead(
+			std::vector<u64>& lookahead,
+			std::vector<u64>& nextLookahead,
+			std::unordered_map<Element, std::unordered_multimap<ID, ID>>& revTransitionMap,
+			std::vector<std::vector<Item>>& closureVec,
+			std::vector<std::pair<Element, std::vector<Element>>>& grammerVec,
+			std::unordered_map<Element, std::vector<u64>>& firstTable,
+			ID closureID, Item item) {
+
+			std::pair<Element, std::vector<Element>>& core = grammerVec[item.prodId];
+			Element mark = core.second[item.dotPos];
+			auto range = revTransitionMap[mark].equal_range(closureID);
+			for (auto it = range.first; it != range.second; ++it) {
+				std::vector<Item>& q = closureVec[it->second];
+				for (auto si = q.begin(); si != q.end(); ++si) {
+					std::vector<Element>& rhs = grammerVec[si->prodId].second;
+
+					if (rhs.size() == si->dotPos + 1)
+						goto first_epsilon;
+
+					{
+						std::vector<u64>& first = firstTable[rhs[si->dotPos + 1]];
+						if (!first.size())
+							goto first_epsilon;
+						if (first[0] & 0b1)
+							goto first_epsilon;
+
+						lookahead.resize(std::max(lookahead.size(), first.size()), 0);
+						for (size_t i = 0; i < first.size(); i++)
+							lookahead[i] |= first[i];
+					}
+
+					continue;
+
+				first_epsilon:
+					lookahead.resize(std::max(lookahead.size(), nextLookahead.size()), 0);
+					for (size_t i = 0; i < nextLookahead.size(); i++)
+						lookahead[i] |= nextLookahead[i];
+				}
+			}
+		}
+
+		void setLookahead(
+			std::vector<std::unordered_set<Element>>& lookaheadSet,
+			std::vector<std::vector<Item>>& c0graph,
+			std::unordered_map<Element, std::unordered_multimap<ID, ID>>& revTransitionMap,
+			std::vector<std::pair<Element, std::vector<Element>>>& grammerVec,
+			std::unordered_multimap<Element, std::pair<std::vector<Element>, u64>>& grammerMap,
+			std::unordered_map<Element, std::vector<u64>>& firstTable) {
+		}
+
+		void createTable(std::vector<std::pair<Element, std::vector<Element>>>& grammerVec) {
 
 		}
 	}
